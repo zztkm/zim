d2fb31c457a73d0a54bfbe45f1053ce7a56ee984
zztkm <33755694+zztkm@users.noreply.github.com>
2025-12-27 23:36:35 +0900

    mode の概念を追加する

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::{
-    fs::write,
-    io::{self, Write, stdout},
-};
+use std::io::{self, Write, stdout};
 use termion::{event::Key, input::TermRead, raw::IntoRawMode};
+
+enum Mode {
+    Normal,
+    Command,
+}
 
 struct Cursor {
     x: u16,
@@ -38,18 +40,23 @@ impl Cursor {
 }
 
 fn draw_rows(stdout: &mut impl Write, rows: u16) -> io::Result<()> {
-    for i in 0..rows {
+    // 最後の行はステータス / コマンドライン用
+    for i in 0..rows - 1 {
         write!(stdout, "~")?;
-
-        // 最後の行以外は改行する
-        if i < rows - 1 {
+        // 最後の行の手前の行以外は改行する
+        if i < rows - 2 {
             write!(stdout, "\r\n")?;
         }
     }
     Ok(())
 }
 
-fn refresh_screen(stdout: &mut impl Write, cursor: &Cursor) -> io::Result<()> {
+fn refresh_screen(
+    stdout: &mut impl Write,
+    cursor: &Cursor,
+    mode: &Mode,
+    command_buffer: &str,
+) -> io::Result<()> {
     // カーソルを隠す
     write!(stdout, "{}", termion::cursor::Hide)?;
     // カーソルを左上に移動
@@ -59,11 +66,34 @@ fn refresh_screen(stdout: &mut impl Write, cursor: &Cursor) -> io::Result<()> {
     let size = termion::terminal_size()?;
     draw_rows(stdout, size.1)?;
 
-    // カーソル移動
-    write!(stdout, "{}", termion::cursor::Goto(cursor.x, cursor.y))?;
-    // カーソル表示
-    write!(stdout, "{}", termion::cursor::Show)?;
+    // コマンドライン / ステータスライン (最下行)
+    write!(stdout, "\r\n")?;
+    match mode {
+        Mode::Command => {
+            write!(stdout, ":{}", command_buffer)?;
+        }
+        Mode::Normal => {
+            write!(stdout, " ")?;
+        }
+    }
 
+    // カーソル位置に移動
+    match mode {
+        Mode::Command => {
+            // コマンドモード時はコマンドライン上にカーソル
+            write!(
+                stdout,
+                ":{}",
+                termion::cursor::Goto((command_buffer.len() + 2) as u16, size.1)
+            )?;
+        }
+        Mode::Normal => {
+            // ノーマルモード時はエディタ上にカーソル
+            write!(stdout, "{}", termion::cursor::Goto(cursor.x, cursor.y))?;
+        }
+    }
+
+    write!(stdout, "{}", termion::cursor::Show)?;
     stdout.flush()?;
     Ok(())
 }
@@ -72,30 +102,62 @@ fn main() -> io::Result<()> {
     // raw mode 切り替え
     let mut stdout = stdout().into_raw_mode()?;
     let mut cursor = Cursor::new();
+    let mut mode = Mode::Normal;
+    let mut command_buffer = String::new();
 
     // 画面クリア
     write!(stdout, "{}", termion::clear::All)?;
     // 初期画面
-    refresh_screen(&mut stdout, &cursor)?;
+    refresh_screen(&mut stdout, &cursor, &mode, &command_buffer)?;
 
     let stdin = io::stdin();
     let size = termion::terminal_size()?;
 
     // キー入力ループ
     for key in stdin.keys() {
-        match key? {
-            // Ctrl + q で終了
-            Key::Ctrl('q') => break,
-            // vim キーバインド
-            Key::Char('h') => cursor.move_left(),
-            Key::Char('j') => cursor.move_down(size.1),
-            Key::Char('k') => cursor.move_up(),
-            Key::Char('l') => cursor.move_right(size.0),
-            _ => {}
+        match mode {
+            Mode::Normal => {
+                match key? {
+                    Key::Char(':') => {
+                        mode = Mode::Command;
+                        command_buffer.clear();
+                    }
+                    // vim キーバインド
+                    Key::Char('h') => cursor.move_left(),
+                    // ステータスライン分 - 1
+                    Key::Char('j') => cursor.move_down(size.1 - 1),
+                    Key::Char('k') => cursor.move_up(),
+                    Key::Char('l') => cursor.move_right(size.0),
+                    _ => {}
+                }
+            }
+            Mode::Command => {
+                match key? {
+                    Key::Char('\n') => {
+                        // コマンド実行
+                        if command_buffer == "q" {
+                            break;
+                        }
+                        // その他のコマンドは今は無視
+                        mode = Mode::Normal;
+                        command_buffer.clear();
+                    }
+                    Key::Esc => {
+                        // コマンドモードをキャンセル
+                        mode = Mode::Normal;
+                        command_buffer.clear();
+                    }
+                    Key::Char(c) => command_buffer.push(c),
+                    Key::Backspace => {
+                        command_buffer.pop();
+                    }
+                    _ => {}
+                }
+            }
         }
 
         // キー入力後に再描画
-        refresh_screen(&mut stdout, &cursor)?;
+        refresh_screen(&mut stdout, &cursor, &mode, &command_buffer)?;
     }
 
     // 終了時に画面をクリアする
